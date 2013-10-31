826c73b2ed6f79b67bb1b60a4d0cfdd2d8276a90
Andrew Fecheyr <andrew@bedesign.be>
2013-10-31 10:49:14 +0100

    Add attribute to turn stats on/off and increase interval time to 60

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,4 +3,5 @@ default['suricata']['log_dir'] = "/var/log/suricata/"
 default['suricata']['ppa'] = "stable"
 default['suricata']['interface'] = "eth0"
 default['suricata']['config_file'] = "/etc/suricata/suricata.yaml"
+default['suricata']['enable_stats'] = true
 
